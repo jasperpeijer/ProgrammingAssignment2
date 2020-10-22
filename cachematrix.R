200b9843bbf2e91fd31d7ff5f6d3417d62a350f1
jasperpeijer <jasperpeijer@hotmail.com>
2020-10-22 16:40:36 +0200

    Added functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+m <- NULL
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if (is.null(m)) {
+    m <<- solve(c)
+  }
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  if (is.null(m)) {
+    return(solve(x))
+  }
+
+  m
 }
